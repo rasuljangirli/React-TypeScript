cbab5f042816e6a098c50002c0373b18126a60bd
rasuljangirli <127443671+rasuljangirli@users.noreply.github.com>
2024-07-03 22:57:04 +0400

    Update App.tsx

--- 3.Generic Types Extending Types (Partial,Requered,ReadOnly,pick,omit)/src/App.tsx
+++ 3.Generic Types Extending Types (Partial,Requered,ReadOnly,pick,omit)/src/App.tsx
@@ -1,6 +1,127 @@
 import "./App.css";
 
 function App() {
+  //! Generic Types
+  function writeClg<T>(arr: T) {
+    console.log(arr);
+  }
+  writeClg([1, 2, 3, 4, 5]);
+  writeClg(["Hello", "How", "are", "You"]);
+  writeClg([true, false]);
+
+  console.log("--------------------------------------------");
+
+
+ 
+  interface GenericType<T> {
+    name: string;
+    age: number;
+    salary: T[];
+  }
+
+  let user_1: GenericType<number> = {
+    name: "Alisa",
+    age: 23,
+    salary: [5000, 2000, 1000],
+  };
+  let user_2: GenericType<string> = {
+    name: "Aloso",
+    age: 32,
+    salary: ["1432", "6572", "1111"],
+  };
+  let arr: GenericType<string | number>[] = [user_1, user_2];
+
+  function writeArr<T>(arr: GenericType<T>[]) {
+    arr.forEach((element: GenericType<T>) => console.log(element));
+  }
+  writeArr(arr);
+  console.log("----------------------------------------------------");
+
+  //? Extends
+ 
+
+  interface Cars {
+    id: number;
+    name: string;
+    year: number;
+  }
+
+  interface Ferrari extends Cars {
+    marka: string;
+  }
+
+  interface RangRover extends Cars {
+    marka: string;
+  }
+
+  let driveCar_1: Ferrari = {
+    id: 1,
+    name: "Ferrari",
+    year: 2030,
+    marka: "NoMark",
+  };
+
+  let driveCar_2: RangRover = {
+    id: 1,
+    name: "Ferrari",
+    year: 2030,
+    marka: "RangRover 2012 Sport",
+  };
+
+  console.log(driveCar_1);
+  console.log(driveCar_2);
+
+  //? Partial , Required, ReadOnly,pick,omit
+
+  interface User {
+    name: string;
+    age: number;
+    salary: number;
+  }
+
+  //! Partical
+ 
+  let User_One: Partial<User> = {
+    name: "Alisa",
+    age: 23,
+  };
+  console.log(User_One);
+
+  //! Required
+  
+  let User_Two: Required<User> = {
+    name: "Alisa",
+    age: 23,
+    salary: 45000,
+  };
+  console.log(User_Two);
+
+  //!ReadOnly
+  
+
+  let User_Tree: Readonly<User> = {
+    name: "Alisa",
+    age: 23,
+    salary: 45000,
+  };
+  //User_Tree.name = "Aloso";
+  console.log(User_Tree);
+
+  
+  let User_Five: Pick<User, "name"> = {
+    name: "Alisa",
+  };
+
+  console.log(User_Five);
+
+ 
+  let User_Last: Omit<User, "name"> = {
+    age: 23,
+    salary: 45000,
+  };
+
+  console.log(User_Last);
+
   return (
     <div>
       <p>You are one :)</p>
